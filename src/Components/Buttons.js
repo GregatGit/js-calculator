6a852b7f20b9f613487cc0eae66ab219a0dd3e67
gregatgit <gregdd@outlook.com>
2019-09-11 23:33:59 +1000

    change ids

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,40 +1,40 @@
 import React from 'react'
 import Button from './Button'
 
-const Buttons = ({func}) => {
+const Buttons = ({ func }) => {
   return (
     <div>
-        <div>
-          <Button value="7" id={7} func={func} />
-          <Button value="8" id={8} func={func} />
-          <Button value="9" id={9} func={func} />
-          <Button value="÷" id={'/'} func={func} />
-        </div>
-        <div>
-          <Button value="4" id={4} func={func} />
-          <Button value="5" id={5} func={func} />
-          <Button value="6" id={6} func={func} />
-          <Button value="X" id={'*'} func={func} />
-        </div>
-        <div>
-          <Button value="1" id={1} func={func} />
-          <Button value="2" id={2} func={func} />
-          <Button value="3" id={3} func={func} />
-          <Button value="-" id={'-'} func={func} />
-        </div>
-        <div>
-          <Button value="0" id={0} func={func} />
-          <Button value="." id={'.'} func={func} />
-          <Button value="=" id={'='} func={func} />
-          <Button value="+" id={'+'} func={func} />
-        </div>
-        <div>
-          <Button value="M" id={'M'} func={func} />
-          <Button value="M+" id={'M+'} func={func} />
-          <Button value="MR" id={'MR'} func={func} />
-          <Button value="MC" id={'MC'} func={func} />
-        </div>
+      <div>
+        <Button value="7" func={func} id="seven" />
+        <Button value="8" func={func} id="eight" />
+        <Button value="9" func={func} id="nine" />
+        <Button value="÷" func={func} id="divide" />
       </div>
+      <div>
+        <Button value="4" func={func} id="four" />
+        <Button value="5" func={func} id="five" />
+        <Button value="6" func={func} id="six" />
+        <Button value="X" func={func} id="multiply" />
+      </div>
+      <div>
+        <Button value="1" func={func} id="one"/>
+        <Button value="2" func={func} id="two"/>
+        <Button value="3" func={func} id="three"/>
+        <Button value="-" func={func} id="minus"/>
+      </div>
+      <div>
+        <Button value="0" func={func} id="zero" />
+        <Button value="." func={func} id="decimal" />
+        <Button value="=" func={func} id="equals" />
+        <Button value="+" func={func} id="plus" />
+      </div>
+      <div>
+        <Button value="M" func={func}  id="memory" />
+        <Button value="M+" func={func} id="memoryPlus" />
+        <Button value="MR" func={func} id="memoryRecall" />
+        <Button value="MC" func={func} id="memoryClear" />
+      </div>
+    </div>
   )
 }
 
