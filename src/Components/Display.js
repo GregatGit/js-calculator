7ea838529a00ba4ae7ae90c0531f148bc8f46ed0
gregatgit <gregdd@outlook.com>
2019-09-11 23:12:31 +1000

    add display and stylings

---
+++ src/Components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div>
+      <input className="c-display" readOnly placeholder="0" type="text"/>
+    </div>
+  )
+}
+
+export default Display
