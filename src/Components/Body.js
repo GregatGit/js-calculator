f48aa897bb3d8d19193c6bb6dc4284949b519a32
gregatgit <gregdd@outlook.com>
2019-09-12 23:22:36 +1000

    all number with more than one digit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,33 +5,56 @@ import './styles.scss'
 
 const Body = () => {
 
-  const [display, setDisplay] = useState('') 
+  const [display, setDisplay] = useState('') // what is shown
   const [equation, setEquation] = useState([])
+  const [join, setJoin] = useState(false) // a flag for nums > 1 char
+  const nums = ['1','2','3','4','5','6','7','8','9', '0', '.']
 
   const buttonPush = str => {
+    let newChar = str // replace str if needed
     if(str === 'clear'){ // reset all
       setDisplay(0)
       setEquation([])
+      setJoin(false)
       return
     }
-    let newChar = str // replace str if needed
-    if (str === '✖'){
-      newChar = '*'
-    }
-    if (str === '÷'){
-      newChar = '/'
-    }
-    if (str === '➖'){
-      newChar = '-'
-    }
-
-    if (str === '='){
+    if (str === '='){ // do the sum once = is pressed
       let arr = [...equation].join('')
       newChar = eval(arr).toFixed(4)
       setDisplay(newChar)
       setEquation([])
+      setJoin(false)
       return
     }
+
+    if (nums.indexOf(str) !== -1){ // if num is pushed
+      let finalNum = str
+      const newArr = [...equation]
+      if (join){
+        let lastNum = newArr.pop()
+        finalNum = lastNum + str 
+      }
+      setEquation([...newArr, finalNum])
+      setJoin(true)
+      setDisplay(finalNum)
+      return
+    }
+
+    
+    
+    if (str === '✖'){
+      newChar = '*'
+      setJoin(false)
+    }
+    if (str === '÷'){
+      newChar = '/'
+      setJoin(false)
+    }
+    if (str === '➖'){
+      newChar = '-'
+      setJoin(false)
+    }
+
     setEquation([...equation, newChar])
     console.log(equation)
     
