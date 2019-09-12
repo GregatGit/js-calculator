d43b0d64e9b3e360fc15185e64731444cdaf8c17
gregatgit <gregdd@outlook.com>
2019-09-12 22:10:55 +1000

    start adding logic to calculator

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -6,12 +6,36 @@ import './styles.scss'
 const Body = () => {
 
   const [input, setInput] = useState('')
+  const [equation, setEquation] = useState([])
 
   const buttonPush = str => {
-    if(str === 'clear'){
+    if(str === 'clear'){ // reset all
       setInput(0)
-    }else {
-    setInput(str)}
+      setEquation([])
+      return
+    }
+    let newChar = str // replace str if needed
+    if (str === '✖'){
+      newChar = '*'
+    }
+    if (str === '÷'){
+      newChar = '/'
+    }
+    if (str === '➖'){
+      newChar = '-'
+    }
+
+    if (str === '='){
+      let arr = [...equation].join('')
+      newChar = eval(arr)
+      setInput(newChar)
+      setEquation([])
+      return
+    }
+    setEquation([...equation, newChar])
+    console.log(equation)
+    
+    setInput(str)
   }
 
   return (
