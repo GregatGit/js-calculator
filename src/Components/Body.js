7494de362ecf944aef517e9b576acf3cf3d6e0b4
gregatgit <gregdd@outlook.com>
2019-09-13 22:55:31 +1000

    start extracting logic from body component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,7 @@
 import React, { useState} from 'react'
 import Buttons from './Buttons'
 import Display from './Display'
+import { nums, symbols, startOfSum } from '../utils'
 import './styles.scss'
 
 const Body = () => {
@@ -8,10 +9,7 @@ const Body = () => {
   const [display, setDisplay] = useState('') // what is shown
   const [equation, setEquation] = useState([])
   const [join, setJoin] = useState(false) // a flag for nums > 1 char
-  const nums = ['1','2','3','4','5','6','7','8','9', '0', '.']
-  const startOfSum =['1','2','3','4','5','6','7','8','9', '.']
-  const symbols = ['-', '+', '*', '/', '=']
-
+  
   const buttonPush = str => {
     let newChar = str // replace str if needed
 
@@ -30,6 +28,12 @@ const Body = () => {
     }
 
     if (str === '='){ // do the sum once = is pressed
+      let checkForSymbol = equation[equation.length - 1]
+      if (symbols.indexOf(checkForSymbol) !== -1){
+        setEquation([...equation, display])
+        console.log(equation)
+        console.log(display)
+      }
       let arr = [...equation].join('')
       newChar = eval(arr).toFixed(4)
       newChar = parseFloat(newChar) // remove trailing 000
---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+export const nums = ['1','2','3','4','5','6','7','8','9', '0', '.']
+export  const startOfSum =['1','2','3','4','5','6','7','8','9', '.']
+export  const symbols = ['-', '+', '*', '/', '=']
+
+function handlePlusMinus(symbol, arr, display) {
+  if (arr.length === 0) return arr
+
+  const lastInArr = arr[arr.length - 1]
+  let newChar = symbol
+  if (newChar === '➖'){
+    newChar = '-'
+  }
+  if (lastInArr === '-' || lastInArr === '+'){
+    arr[arr.length - 1] = newChar 
+    return arr
+  }
+  return [...arr, newChar]
+}
