9922483935fb9991d14c5ba68f8d64a8a9ac1ec0
gregatgit <gregdd@outlook.com>
2019-09-15 22:59:49 +1000

    add display to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import equation from './reducer_equation'
 import lastPressed from './reducer_lastPressed'
+import myDisplay from './reducer_myDIsplay'
 
 const rootReducer = combineReducers({
   equation,
-  lastPressed
+  lastPressed,
+  myDisplay
 })
 
 export default rootReducer
