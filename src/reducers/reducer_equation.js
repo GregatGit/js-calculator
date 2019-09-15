7f5723d8bd97b65795876c1142684b67be1887fa
gregatgit <gregdd@outlook.com>
2019-09-15 22:54:07 +1000

    update reducer

--- src/reducers/reducer_equation.js
+++ src/reducers/reducer_equation.js
@@ -1,10 +1,9 @@
-
+import { BUTTON_PRESSED } from '../actions'
 
 export default function (state = [], action) {
   switch (action.type) {
-    case "value":
-      
-      break;
+    case BUTTON_PRESSED:
+        return [...state, action.payload]
   
     default:
       return state
