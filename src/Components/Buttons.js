8989b3236d7e55d0e1b5beac41f265ba4bf24ce7
gregatgit <gregdd@outlook.com>
2019-09-15 22:52:15 +1000

    use simple symbles eg * not x

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -14,13 +14,13 @@ const Buttons = ({ func }) => {
         <Button value="4" func={func} id="four" />
         <Button value="5" func={func} id="five" />
         <Button value="6" func={func} id="six" />
-        <Button value="✖" func={func} id="multiply" />
+        <Button value="*" func={func} id="multiply" />
       </div>
       <div>
         <Button value="1" func={func} id="one"/>
         <Button value="2" func={func} id="two"/>
         <Button value="3" func={func} id="three"/>
-        <Button value="➖" func={func} id="subtract"/>
+        <Button value="-" func={func} id="subtract"/>
       </div>
       <div>
         <Button value="0" func={func} id="zero" />
