51f8b7ab3fbeca1ee8abdfd873d801bff4fe6b19
gregatgit <gregdd@outlook.com>
2019-09-15 22:54:28 +1000

    create reducer from last button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import equation from './reducer_equation'
+import lastPressed from './reducer_lastPressed'
 
 const rootReducer = combineReducers({
   equation,
+  lastPressed
 })
 
 export default rootReducer
