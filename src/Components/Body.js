0e8b2b91d11cf9d3724ca8b124c811758d8182fb
gregatgit <gregdd@outlook.com>
2019-09-16 15:51:16 +1000

    use evalute from mathjs instead of eval

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,41 +1,51 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
+import { evaluate } from 'mathjs'
 import Buttons from './Buttons'
 import Display from './Display'
 import { nums, symbols, startOfSum } from '../utils'
 import './styles.scss'
 
 const Body = () => {
-
   const [display, setDisplay] = useState('') // what is shown
   const [equation, setEquation] = useState([])
   const [join, setJoin] = useState(false) // a flag for nums > 1 char
-  
+
   const buttonPush = str => {
     let newChar = str // replace str if needed
 
-    if(str === 'clear'){ // reset all
+    if (str === 'clear') {
+      // reset all
       console.log('boom')
       setDisplay(0)
       setEquation([])
       setJoin(false)
       return
     }
-    if (equation.length === 0){ // must start with one of these
-      if (startOfSum.indexOf(str) === -1) return 
-    }
-    if (str === '0'){// make sure 0 is ok no 00
-      if(equation[equation.length - 1] === '0' || equation.length === 0) return
+
+    if (str === '.') { // no extra decimals
+      if (equation.length > 0) {
+        let temp = equation[equation.length - 1]
+        if (temp.indexOf('.') !== -1) return
+      }
     }
 
-    if (str === '='){ // do the sum once = is pressed
+    if (equation.length === 0) {
+      // must start with one of these
+      if (startOfSum.indexOf(str) === -1) return
+    }
+    if (str === '0') {
+      // make sure 0 is ok no 00
+      if (equation[equation.length - 1] === '0' || equation.length === 0) return
+    }
+
+    if (str === '=') {
+      // do the sum once = is pressed
       let checkForSymbol = equation[equation.length - 1]
-      if (symbols.indexOf(checkForSymbol) !== -1){
+      if (symbols.indexOf(checkForSymbol) !== -1) {
         setEquation([...equation, display])
-        console.log(equation)
-        console.log(display)
       }
       let arr = [...equation].join('')
-      newChar = eval(arr).toFixed(4)
+      newChar = evaluate(arr).toFixed(4)
       newChar = parseFloat(newChar) // remove trailing 000
       newChar = newChar.toString() // turn back to string
       setDisplay(newChar)
@@ -44,16 +54,13 @@ const Body = () => {
       return
     }
 
-    if (str === '.'){ // deal with decimals
-      
-    }
-
-    if (nums.indexOf(str) !== -1){ // if num is pushed
+    if (nums.indexOf(str) !== -1) {
+      // if num is pushed
       let finalNum = str
       const newArr = [...equation]
-      if (join){
+      if (join) {
         let lastNum = newArr.pop()
-        finalNum = lastNum + str 
+        finalNum = lastNum + str
       }
       setEquation([...newArr, finalNum])
       setJoin(true)
@@ -61,19 +68,19 @@ const Body = () => {
       return
     }
 
-    if (str === '+'){
+    if (str === '+') {
       setJoin(false)
     }
-    
-    if (str === '✖'){
+
+    if (str === '✖') {
       newChar = '*'
       setJoin(false)
     }
-    if (str === '÷'){
+    if (str === '÷') {
       newChar = '/'
       setJoin(false)
     }
-    if (str === '➖'){
+    if (str === '➖') {
       newChar = '-'
       setJoin(false)
     }
@@ -87,9 +94,8 @@ const Body = () => {
   return (
     <div className="c-body">
       <h1>JS Calulator</h1>
-      <Display display={display}/>
+      <Display display={display} />
       <Buttons func={buttonPush} />
-      
     </div>
   )
 }
